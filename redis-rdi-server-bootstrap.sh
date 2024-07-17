643496a995a90627fadeec35da2b39af19246aa0
Gabriel Cerioni <gacerioni@gmail.com>
2024-07-17 16:16:32 -0300

    Update redis-rdi-server-bootstrap.sh

--- redis-rdi-server-bootstrap.sh
+++ redis-rdi-server-bootstrap.sh
@@ -6,7 +6,7 @@
 
 # Variables
 FILES=(
-    "https://s3.amazonaws.com/redis-latam-rdi-poc-deps/rdi-installation-1.2.1tar.gz"
+    "https://s3.amazonaws.com/redis-latam-rdi-poc-deps/rdi-installation-1.2.1.tar.gz"
     "https://s3.amazonaws.com/redis-latam-rdi-poc-deps/redislabs-7.4.2-216-focal-amd64.tar"
 )
 DEST_DIR="/root"
